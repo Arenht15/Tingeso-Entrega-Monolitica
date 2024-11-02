07dcf16ceb8c83f511f06a8029e2887598c532a8
Sara Salas <sara.salas@usach.cl>
2024-11-02 00:41:50 -0300

    prueba2

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
                     Bienvenido a Presta Banco
                 </Typography>
                 <Typography variant="h5" style={{color: '#555'}}>
-                    El Banco Ideal Para Realizar Préstamos.
+                    El Banco Ideal Para Realizar Préstamos Más Confiable.
                 </Typography>
             </Container>
             <style>{`
